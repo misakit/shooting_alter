2fcc6a84c4046f089e575663a60982c55a9749e5
TOYOTA Misaki <i@misakit.com>
2014-08-05 17:22:28 +0900

    first commit: Map, Pad, Player の作成

---
+++ game.js
@@ -0,0 +1,65 @@
+enchant();
+
+window.onload = function() {
+  var game = new Game(320, 320);
+  game.fps = 30;
+  game.preload('map0.png', 'chara7.png', 'icon0.png', 'pad.png');
+  game.onload = function() {
+    /*
+       Map の作成
+    */
+    var blocks = [
+      [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
+      [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+      [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+      [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+      [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+      [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+      [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+      [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+      [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+      [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+      [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+      [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+      [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    ];
+    var map = new Map(16, 16);
+    map.image = game.assets['map0.png'];
+    map.loadData(blocks);
+
+    var player = new Sprite(32, 32);
+    player.frame = 27;
+    player.image = game.assets['chara7.png'];
+    player.addEventListener('enterframe', function(e) {
+      if (game.input.up) {
+        if (game.frame % 3 == 0) {
+          this.frame++;
+        };
+      };
+    });
+
+    /*
+       操作 Pad の作成。
+     */
+    var pad = new Pad();
+    pad.x = 10;
+    pad.y = 215;
+    pad.addEventListener('enterframe', function(e) {
+      pad.frame = 0;
+      if (game.input.right) {
+        pad.frame = 1;
+        pad.rotation = 90;
+      };
+      if (game.input.left) {
+        pad.frame = 1;
+        pad.rotation = -90;
+      };
+    });
+
+    game.rootScene.addChild(map);
+    game.rootScene.addChild(player);
+    game.rootScene.addChild(pad);
+    game.rootScene.backgroundColor = 'rgb(182, 255, 255)';
+  };
+  game.start();
+}
